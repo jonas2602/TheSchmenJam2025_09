5c622c433bd8220beefede5684063080531a7b7f
Muhammad Inshal Uddin <muhammad.uddin@cloudimperiumgames.com>
2025-09-13 17:24:06 +0200

    Add conquering logic for tiles

--- scenes/map_controller.gd
+++ scenes/map_controller.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
+@export var base_capture_time : float = 3.0
+
 @onready var tile_map_layer = $TileMapLayer
 
 
 func _input(event):
 	# need to detect mouse over
-	if 	event is InputEventMouseMotion:
+	if event is InputEventMouseMotion:
 		#print("Mouse is at: ", event.position)
 		var tile_pos	 = tile_map_layer.local_to_map(event.position)
 		if (tile_pos != GlobalEventSystem.hovered_tile_pos):
@@ -15,11 +17,54 @@ func _input(event):
 	
 	var is_left_click_pressed = event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT
 	if is_left_click_pressed:
-		var tile_pos	 = tile_map_layer.local_to_map(event.position)
-		
+		var tile_pos  = tile_map_layer.local_to_map(event.position)
+		var tile_data = tile_map_layer.get_cell_tile_data(tile_pos)
+		if not tile_data:
+			return # not a valid cell
+			
+		# start conquering the tile if not already in progress
+		_start_conquering(tile_pos, tile_data)
+
 		# emit tile click event
 		if (tile_pos != GlobalEventSystem.last_clicked_tile_pos):
 			#print("Tile ", tile_pos, " is clicked")
 			GlobalEventSystem.clicked_tile.emit(tile_pos)
 		pass
+	pass
+
+
+func _start_conquering(tile_pos : Vector2i, tile_data : TileData):
+	
+	if tile_pos in GlobalEventSystem.conquering_tile_timers:
+		return # already being conquered
+		
+	if tile_data.get_custom_data("conquered"):
+		print("Tile: ", tile_pos, " is already conquered!")
+		return # already conquered
+	
+	print("Beginning to conquer tile: ", tile_pos)
+
+	var conquered_timer = Timer.new()
+	add_child(conquered_timer)
+	conquered_timer.wait_time = base_capture_time
+	conquered_timer.timeout.connect(func(): self._conquer_tile(tile_pos, tile_data))
+	conquered_timer.one_shot = true
+	conquered_timer.start()
+	
+	# add the timer to global list so it can be aborted
+	GlobalEventSystem.conquering_tile_timers[tile_pos] = conquered_timer
+	pass
+
+
+func _conquer_tile(tile_pos : Vector2i, tile_data : TileData):
+	# TODO: Pool the timers somehow?
+	GlobalEventSystem.conquering_tile_timers[tile_pos].queue_free()
+	GlobalEventSystem.conquering_tile_timers.erase(tile_pos)
+	
+	tile_data.set_custom_data("conquered", true)
+	
+	# TODO: modify tile map layer
+	
+	GlobalEventSystem.conquered_tile.emit(tile_pos)
+	
 	pass
--- scripts/global_event_system.gd
+++ scripts/global_event_system.gd
@@ -1,15 +1,21 @@
 extends Node
+	
+static var hovered_tile_pos        = Vector2i(255, 255)
+static var last_clicked_tile_pos   = Vector2i(255, 255)
+static var last_conquered_tile_pos = Vector2i(255, 255)
+static var conquering_tile_timers  = {}
 
-static var hovered_tile_pos      = Vector2i(255, 255)
-static var last_clicked_tile_pos = Vector2i(255, 255)
 
 signal hovered_tile_changed(tile_pos : Vector2i)
 signal clicked_tile(tile_pos : Vector2i)
+signal conquered_tile(tile_pos : Vector2i)
 
+# Various handlers to cache the state global state for other scripts
 
 func _init() -> void:
 	hovered_tile_changed.connect(_on_hovered_tile_changed)
 	clicked_tile.connect(_on_tile_clicked)
+	conquered_tile.connect(_on_tile_conquered)
 	pass
 
 func _on_hovered_tile_changed(tile_pos : Vector2i):
@@ -20,4 +26,9 @@ func _on_hovered_tile_changed(tile_pos : Vector2i):
 func _on_tile_clicked(tile_pos : Vector2i):
 	print("Last clicked tile changed from ", last_clicked_tile_pos, " to ", tile_pos)
 	last_clicked_tile_pos = tile_pos
+	pass
+
+func _on_tile_conquered(tile_pos : Vector2i):
+	print("Last conquered tile changed from ", last_conquered_tile_pos, " to ", tile_pos)
+	last_conquered_tile_pos = tile_pos
 	pass
