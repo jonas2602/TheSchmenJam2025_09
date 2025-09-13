253fc22f9121fd4d7b1e4202360bc168d29f8f5e
Muhammad Inshal Uddin <muhammad.uddin@cloudimperiumgames.com>
2025-09-13 16:48:33 +0200

    Add support for clicking tiles

--- scenes/map_controller.gd
+++ scenes/map_controller.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 
 func _input(event):
-# need to detect mouse over
+	# need to detect mouse over
 	if 	event is InputEventMouseMotion:
 		#print("Mouse is at: ", event.position)
 		var tile_pos	 = tile_map_layer.local_to_map(event.position)
@@ -13,5 +13,13 @@ func _input(event):
 			GlobalEventSystem.hovered_tile_changed.emit(tile_pos)
 	pass
 	
-
-# need to modify the grid state
+	var is_left_click_pressed = event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT
+	if is_left_click_pressed:
+		var tile_pos	 = tile_map_layer.local_to_map(event.position)
+		
+		# emit tile click event
+		if (tile_pos != GlobalEventSystem.last_clicked_tile_pos):
+			#print("Tile ", tile_pos, " is clicked")
+			GlobalEventSystem.clicked_tile.emit(tile_pos)
+		pass
+	pass
--- scripts/global_event_system.gd
+++ scripts/global_event_system.gd
@@ -1,15 +1,23 @@
 extends Node
 
-static var hovered_tile_pos = Vector2i(255, 255)
+static var hovered_tile_pos      = Vector2i(255, 255)
+static var last_clicked_tile_pos = Vector2i(255, 255)
 
 signal hovered_tile_changed(tile_pos : Vector2i)
 signal clicked_tile(tile_pos : Vector2i)
 
+
 func _init() -> void:
 	hovered_tile_changed.connect(_on_hovered_tile_changed)
+	clicked_tile.connect(_on_tile_clicked)
 	pass
 
 func _on_hovered_tile_changed(tile_pos : Vector2i):
 	print("Changing hovered tile from ", hovered_tile_pos, " to ", tile_pos)
 	hovered_tile_pos = tile_pos
+	pass
+
+func _on_tile_clicked(tile_pos : Vector2i):
+	print("Last clicked tile changed from ", last_clicked_tile_pos, " to ", tile_pos)
+	last_clicked_tile_pos = tile_pos
 	pass
